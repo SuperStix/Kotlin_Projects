8f97f95da25a360fb374c332c9024103e894f3a0
SuperStix <gutierrezdanyfer@gmail.com>
2020-01-07 23:43:08 +0800

    Initial commit

---
+++ app/src/main/java/com/ddg/kotlin_snippet/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ddg.kotlin_snippet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
